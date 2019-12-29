51eff0a1d0aa65023d09d18499355d13d36100b3
Dominic Gerweck <dominic@gerweck2.de>
2019-12-29 12:41:29 +0100

    Day 20: initialize g2Asteroids (WPF GameApp)

---
+++ g2Asteroids/Classes/Asteroid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace g2Asteroids
+{
+    class Asteroid : g2AsteroidsObject
+    {
+        static readonly Random zufällig = new Random();
+
+        //Konstruktor der Kindklasse
+        public Asteroid(Canvas zeichenfläche)
+                : base(0.5, 0.5, (zufällig.NextDouble() - 0.5) * 250, (zufällig.NextDouble() - 0.5) * 250)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                double alpha = 2.0 * Math.PI / 15 * i;
+                double radius = 25.0 + 15.0 * zufällig.NextDouble();
+                umriss.Points.Add(new Point(radius * Math.Cos(alpha), radius * Math.Sin(alpha)));
+            }
+            umriss.StrokeThickness = 5;
+            umriss.Stroke = Brushes.LightBlue;
+            umriss.Fill = Brushes.Gray;
+            //ImageBrush img = new ImageBrush
+            //{
+            //    ImageSource = new BitmapImage(new Uri(@"C:\Users\Nikolaus\source\repos\g2Asteroids\g2Asteroids\images\mf.jpg", UriKind.Relative))
+            //};
+        }
+
+        //überschriebene (override) Methoden der Kindklasse
+        public override void ZeichneObjekt(Canvas zeichenfläche)
+        {
+
+            // TODO : Dies hier jeweils in Mutterklasse einmalig in der Mutterklasse einfügen? virtuell abstract?
+            zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, X);
+            Canvas.SetTop(umriss, Y);
+        }
+
+        public bool Collided(double x, double y)
+        {
+            return umriss.RenderedGeometry.FillContains(new Point(x - X, y - Y));
+        }
+    }
+}
---
+++ g2Asteroids/Classes/Game1Object.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace g2Asteroids
+{
+    abstract class g2AsteroidsObject
+    {
+        //private Eigenschaften der Mutterklasse
+        double x;
+        double y;
+        double vx;
+        double vy;
+        protected Polygon umriss = new Polygon();
+
+        //öffentliche Eigenschaften der Mutterklasse
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+        public double VX { get { return vx; } set { vx = value; } }
+        public double VY { get { return vy; } set { vy = value; } }
+        
+        //Konstruktoren der Mutterklasse
+        public g2AsteroidsObject(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public g2AsteroidsObject(double x, double y, double vx, double vy)
+        {
+            this.x = x;
+            this.y = y;
+            this.vx = vx;
+            this.vy = vy;            
+        }
+
+        //Methoden der Mutterklasse
+        public abstract void ZeichneObjekt(Canvas zeichenfläche);
+
+        // true wenn uber den Rande gegangen
+        public bool AnimiereObjekt(TimeSpan intervall, Canvas zeichenfläche)
+        {
+            x += vx * intervall.TotalSeconds;
+            y += vy * intervall.TotalSeconds;
+
+            //TODO auf WIEDERAPPRALLEN setzen? Video "erste WPF" 
+            bool goneOverTheEdge = false;
+
+            if (x < 0.0)
+            {
+                goneOverTheEdge = true;
+                x = zeichenfläche.ActualWidth;
+
+            }
+            else if (x > zeichenfläche.ActualWidth)
+            {
+                goneOverTheEdge = true;
+                x = 0;
+
+            }
+            if (y < 0.0)
+            {
+                goneOverTheEdge = true;
+                y = zeichenfläche.ActualHeight;
+
+            }
+            else if (y > zeichenfläche.ActualHeight)
+            {
+                goneOverTheEdge = true;
+                y = 0;
+
+            }
+            return goneOverTheEdge;
+        }
+    }
+}
+
+
+
