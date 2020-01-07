1ecf6070f9239f62ec56e0febf7297290f04a87a
Dominic Gerweck <doger83@users.noreply.github.com>
2020-01-07 18:24:04 +0100

    initial commit

--- Classes/Asteroid.cs
+++ Classes/Asteroid.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace g2Asteroids
 {
@@ -16,28 +11,23 @@ namespace g2Asteroids
 
         //Konstruktor der Kindklasse
         public Asteroid(Canvas zeichenfläche)
-                : base(0.5, 0.5, ( zufällig.NextDouble() - 0.5 ) * 250, ( zufällig.NextDouble() - 0.5 ) * 250)
+                : base(0.5, 0.5, ( zufällig.NextDouble() - 0.5 ) * 250,
+                                 ( zufällig.NextDouble() - 0.5 ) * 250)
         {
             for (int i = 0 ; i < 15 ; i++)
             {
                 double alpha = 2.0 * Math.PI / 15 * i;
                 double radius = 25.0 + 15.0 * zufällig.NextDouble();
-                umriss.Points.Add(new Point(radius * Math.Cos(alpha), radius * Math.Sin(alpha)));
+                umriss.Points.Add(new Point(radius * Math.Cos(alpha),
+                                            radius * Math.Sin(alpha)));
             }
             umriss.StrokeThickness = 5;
             umriss.Stroke = Brushes.LightBlue;
             umriss.Fill = Brushes.Gray;
-            //ImageBrush img = new ImageBrush
-            //{
-            //    ImageSource = new BitmapImage(new Uri(@"C:\Users\Nikolaus\source\repos\g2Asteroids\g2Asteroids\images\mf.jpg", UriKind.Relative))
-            //};
         }
 
-        //überschriebene (override) Methoden der Kindklasse
         public override void ZeichneObjekt(Canvas zeichenfläche)
         {
-
-            // TODO : Dies hier jeweils in Mutterklasse einmalig in der Mutterklasse einfügen? virtuell abstract?
             zeichenfläche.Children.Add(umriss);
             Canvas.SetLeft(umriss, X);
             Canvas.SetTop(umriss, Y);
--- Classes/g2AsteroidsObject.cs
+++ Classes/g2AsteroidsObject.cs
@@ -1,50 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace g2Asteroids
 {
     abstract class g2AsteroidsObject
     {
-        //private Eigenschaften der Mutterklasse
         double x;
         double y;
         double vx;
         double vy;
         protected Polygon umriss = new Polygon();
 
-        //öffentliche Eigenschaften der Mutterklasse
-        public double X { get { return x; } set { x = value; } }
-        public double Y { get { return y; } set { y = value; } }
-        public double VX { get { return vx; } set { vx = value; } }
-        public double VY { get { return vy; } set { vy = value; } }
-        
-        //Konstruktoren der Mutterklasse
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public double VX
+        {
+            get { return vx; }
+            set { vx = value; }
+        }
+        public double VY
+        {
+            get { return vy; }
+            set { vy = value; }
+        }
+
         public g2AsteroidsObject(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
 
-        public g2AsteroidsObject(double x, double y, double vx, double vy)
+        public g2AsteroidsObject(double x, double y,
+                                 double vx, double vy)
         {
             this.x = x;
             this.y = y;
             this.vx = vx;
-            this.vy = vy;            
+            this.vy = vy;
         }
 
-        //Methoden der Mutterklasse
         public abstract void ZeichneObjekt(Canvas zeichenfläche);
 
-        // true wenn uber den Rande gegangen
         public bool AnimiereObjekt(TimeSpan intervall, Canvas zeichenfläche)
         {
             x += vx * intervall.TotalSeconds;
